da701f64f7220173f3577cc9c2fa29d946c3f888
nguen <104498646+gawgua@users.noreply.github.com>
2022-08-23 18:25:45 +0700

    Add files via upload

---
+++ cmdidCSVextract.py
@@ -0,0 +1,30 @@
+"""
+    convert cmdid.csv to packetIds.json
+    usage:
+        py cmdidCSVextract.py <csv-file> <(optional)output json file>
+"""
+
+import csv
+import json
+from sys import argv
+
+def convert(csv_path: str, json_path='./packetIds.json'):
+    result = {'13371337': 'PacketHead'}
+
+    with open(csv_path, 'r') as csv_cmdid:
+        csv_rows = csv.DictReader(csv_cmdid)
+        for row in csv_rows:
+            packetName = row['Name']
+            cmdid = row['ID']
+            result[cmdid] = packetName
+
+    with open(json_path, 'w') as json_cmdid:
+        json.dump(result, json_cmdid, indent=4)
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        convert(argv[1])
+    elif len(argv) == 3:
+        convert(argv[1], argv[2])
+    else:
+        print("Invalid arguments")
